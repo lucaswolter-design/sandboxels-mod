8174e218dbd8720fdf929493991dff889a025f80
lucaswolter-design <lucas.wolter@alunoeleva.com.br>
2026-09-14 09:32:22 -0300

    Update murder-drones.js

--- murder-drones.js
+++ murder-drones.js
@@ -1,9 +1,1 @@
-elements.worker_drone = {
-    color: "#808080",
-    behavior: behaviors.POWDER,
-    category: "land",
-    state: "solid",
-    density: 1500,
-};
-
-console.log("MURDER DRONES MOD LOADED");
+console.log("MURDER DRONES TESTE FUNCIONOU!");
