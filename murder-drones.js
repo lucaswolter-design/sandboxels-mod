1802c7071e3b9daaf60d3dc9302999afcf8673fa
lucaswolter-design <lucas.wolter@alunoeleva.com.br>
2026-09-14 09:17:07 -0300

    Update murder-drones.js

--- murder-drones.js
+++ murder-drones.js
@@ -1,12 +1,6 @@
-// ==========================================
-// MURDER DRONES - SANDBOXELS
-// ==========================================
-
 elements.worker_drone = {
-    name: "Worker Drone",
     color: "#808080",
     behavior: behaviors.POWDER,
     category: "Murder Drones",
-    state: "solid",
-    density: 1500
+    state: "solid"
 };
